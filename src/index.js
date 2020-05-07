781aee387036c34a27ee6930c230a7d95662c9ce
Jayden Shaw <jayden.shaw@gmail.com>
2020-05-07 13:07:42 -0600

    Introducting Redux to the app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from 'store';
 import App from 'components/App';
 import GlobalStyles from 'styles/globals';
 import registerServiceWorker from './registerServiceWorker';
 
 const renderApp = () => {
+  const store = configureStore({});
+
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('Initial state ->');
+    console.log(store.getState());
+  }
+
   ReactDOM.render(
-    <div>
-      <GlobalStyles />
-      <App />
-    </div>,
+    <Provider store={store}>
+      <div>
+        <GlobalStyles />
+        <App />
+      </div>
+    </Provider>,
     document.getElementById('root'),
   );
 };
