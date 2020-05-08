5de83c85f9b4b394c0181c7c52628d563d12c4cd
Jayden Shaw <jayden.shaw@gmail.com>
2020-05-08 10:25:36 -0600

    implementing loading animation

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,10 @@
 import { connect } from 'react-redux';
 import actions from 'store/story/actions';
+import { hasMoreStoriesSelector } from 'store/story/selectors';
 import App from './App';
 
 const mapStateToProps = state => ({
+  hasMoreStories: hasMoreStoriesSelector(state),
   stories: state.story.stories,
   page: state.story.page,
   storyIds: state.story.storyIds,
@@ -11,6 +13,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   fetchStoriesFirstPage: () => dispatch(actions.fetchStoryIds()),
+  fetchStories: ({ storyIds, page }) => dispatch(actions.fetchStories({ storyIds, page })),
 });
 
 export default connect(
