5aa918e87f2298b00495fd310120ee849d0fb9e8
Jayden Shaw <jayden.shaw@gmail.com>
2020-05-08 09:30:19 -0600

    Components/App- App.js updating with props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,13 +6,18 @@ import { coloursDark } from 'styles/palette';
 import { Wrapper, Title } from './styles';
 
 class App extends Component {
+    componenetDidMount() {
+        this.props.fetchStoriesFirstPage();
+    }
+
   render() {
+    const { stories } = this.props;
     return (
       <ThemeProvider theme={coloursDark}>
         <div>
           <Wrapper>
             <Title>Hacker News Reader</Title>
-            <List />
+            <List stories={stories}/>
           </Wrapper>
         </div>
       </ThemeProvider>
